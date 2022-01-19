f863bf916c99585aa85dbc461fe456ea43cc9d0b
pankaj kore <korep95@gmail.com>
2022-01-19 21:41:13 +0530

    [pankaj]added calculation of full day wage

--- src/empwage/empwage.java
+++ src/empwage/empwage.java
@@ -1,15 +1,22 @@
 package empwage;
 
-import java.util.Random;
+import java.util.*;
 
 public class empwage {
 	public static void main (String []args) {
 		System.out.println("welcome to employee wage computation");
+		int emp_wage_per_hour=20;
+		int fullDay_hour=8;
 		Random random=new Random();
 		int number=random.nextInt(2);
-		if(number==0)
-			System.out.println("Employee is present");
+		if(number==1)
+			{
+				int income=emp_wage_per_hour*fullDay_hour;
+				System.out.println("Employee is present and full day earning is="+income);
+			}
 		else
-			System.out.println("employee is absent");
+			{
+				System.out.println("employee is absent");
+			}
 		}
 }
