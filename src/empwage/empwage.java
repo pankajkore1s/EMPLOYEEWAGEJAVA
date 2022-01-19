e6be5860fd75e08a679adbbe733beffedc3601f8
pankaj kore <korep95@gmail.com>
2022-01-19 23:42:25 +0530

    [pankaj]added calculation of empWage for month

--- src/empwage/empwage.java
+++ src/empwage/empwage.java
@@ -5,6 +5,7 @@ import java.util.*;
 public class empwage {
 	public static void main (String []args) {
 		System.out.println("welcome to employee wage computation");
+		int Working_day_per_month=20;
 		int Is_Full_Time=1;
 		int Is_Part_Time=0;
 		int emp_wage_per_hour=20;
@@ -16,11 +17,11 @@ public class empwage {
 		switch(number)
 		{
 		case 1:
-				int income=emp_wage_per_hour*fullDay_hour;
+				int income=emp_wage_per_hour*fullDay_hour*Working_day_per_month;
 				System.out.println("Employee is present and full day earning is="+income);
 				break;
 		case 2:
-				int income1=emp_wage_per_hour*parttime_hour;
+				int income1=emp_wage_per_hour*parttime_hour*Working_day_per_month;
 				System.out.println("Employee Is Present And Part Time Day Earning Is="+income1);
 				break;
 		default:
