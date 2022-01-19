2ff059657b21eb19a5b5f95f71a9eba214673ea2
pankaj kore <korep95@gmail.com>
2022-01-19 19:05:35 +0530

    [pankaj]added employee attendance

--- src/empwage/empwage.java
+++ src/empwage/empwage.java
@@ -1,7 +1,15 @@
 package empwage;
 
+import java.util.Random;
+
 public class empwage {
 	public static void main (String []args) {
 		System.out.println("welcome to employee wage computation");
-	}
+		Random random=new Random();
+		int number=random.nextInt(2);
+		if(number==0)
+			System.out.println("Employee is present");
+		else
+			System.out.println("employee is absent");
+		}
 }
