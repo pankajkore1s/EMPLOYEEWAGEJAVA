2e8200865f71e469fe4d79ad7366f6ef2f1bb456
pankaj kore <korep95@gmail.com>
2022-01-19 23:28:06 +0530

    [pankaj]calculation using switch case

--- src/empwage/empwage.java
+++ src/empwage/empwage.java
@@ -11,17 +11,22 @@ public class empwage {
 		int fullDay_hour=8;
 		int parttime_hour=4;
 		Random random=new Random();
-		int number=random.nextInt(2);
-		if(number==Is_Full_Time)
+		int number=random.nextInt(3);
+		System.out.println(number);
+		switch(number)
 		{
-			int income=emp_wage_per_hour*fullDay_hour;
-			System.out.println("Employee is present and full day earning is="+income);
+		case 1:
+				int income=emp_wage_per_hour*fullDay_hour;
+				System.out.println("Employee is present and full day earning is="+income);
+				break;
+		case 2:
+				int income1=emp_wage_per_hour*parttime_hour;
+				System.out.println("Employee Is Present And Part Time Day Earning Is="+income1);
+				break;
+		default:
+				System.out.println("Employee is absent hence there will be no earning");
 		}
-		else
-		{
-			int income=emp_wage_per_hour*parttime_hour;
-			System.out.println("Employee Is Present And Part Time Day Earning Is="+income);
-		}
+		
 	}
 }
 
